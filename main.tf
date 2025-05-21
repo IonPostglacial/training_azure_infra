8d9e0b603f16caa57ac01f30e224ae66564ea860
Ubuntu <student@nicolas-enablon.1nkoirkcj1cuzicxtpoxj5ixyf.parx.internal.cloudapp.net>
2025-05-21 13:59:19 +0000

    fix formating of main.tf

--- main.tf
+++ main.tf
@@ -21,13 +21,13 @@ locals {
       vnet_address_space    = "10.0.0.0/16"
       subnet_name           = "ngalipot-subnet1"
       subnet_address_prefix = "10.0.1.0/24"
-      open_ports = ["80", "443"]
+      open_ports            = ["80", "443"]
     },
     { vnet_name             = "ngalipot-vnet2"
       vnet_address_space    = "10.1.0.0/16"
       subnet_name           = "ngalipot-subnet2"
       subnet_address_prefix = "10.1.1.0/24"
-      open_ports = ["80", "443"]
+      open_ports            = ["80", "443"]
     },
   ]
 }
@@ -40,14 +40,14 @@ resource "azurerm_resource_group" "ngalipot_rg" {
 module "azure_rg" {
   count = length(local.vnets)
 
-  source                  = "./modules/azure_rg"
-  subscription_id         = local.subscription_id
-  resource_group          = { name     = local.resource_group_name
-                              location = local.resource_group_location
-                            }
-  vnet_name               = local.vnets[count.index].vnet_name
-  vnet_address_space      = local.vnets[count.index].vnet_address_space
-  subnet_name             = local.vnets[count.index].subnet_name
-  subnet_address_prefix   = local.vnets[count.index].subnet_address_prefix
-  open_ports              = local.vnets[count.index].open_ports
+  source          = "./modules/azure_rg"
+  subscription_id = local.subscription_id
+  resource_group = { name = local.resource_group_name
+    location = local.resource_group_location
+  }
+  vnet_name             = local.vnets[count.index].vnet_name
+  vnet_address_space    = local.vnets[count.index].vnet_address_space
+  subnet_name           = local.vnets[count.index].subnet_name
+  subnet_address_prefix = local.vnets[count.index].subnet_address_prefix
+  open_ports            = local.vnets[count.index].open_ports
 }
